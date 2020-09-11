b2d26df2db82fcd418f2c20d0b4d02d41e5238f8
Benjamin-Davies <bentendavies@gmail.com>
2020-09-12 10:34:27 +1200

    Move functionality to separate functions

--- index.ts
+++ index.ts
@@ -3,19 +3,28 @@ import { promises as fs } from 'fs';
 const template = 'template.md';
 const destFile = 'README.md';
 
+function getSchoolYear() {
+  const schoolYear = new Date().getFullYear() - 2008;
+  return `${schoolYear}`;
+}
+
+function replaceSections(sections: { [_: string]: string }, input: string): string {
+  return input.replace(
+    /<!-- PLACEHOLDER:(\w+) -->/g,
+    (_, name: string) => sections[name] ?? `<ERROR: section '${name}' not found>`
+  );
+}
+
 async function run() {
-  const sections: { [_: string]: string } = {
-    YEAR: `${new Date().getFullYear() - 2008}`,
+  const sections = {
+    YEAR: getSchoolYear(),
     REPOS: 'Coming soon...',
   };
 
   // Keeping everything in memory should be fine
   // because the file size will be small
   const input = await fs.readFile(template, { encoding: 'utf8' });
-  const output = input.replace(
-    /<!-- PLACEHOLDER:(\w+) -->/g,
-    (_, name: string) => sections[name] ?? `<ERROR: section '${name}' not found>`
-  );
+  const output = replaceSections(sections, input);
   await fs.writeFile(destFile, output);
 }
 
