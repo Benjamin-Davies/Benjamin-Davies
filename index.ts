4cb163eb8005b9bc9e73d5313dcca2f47176e875
Benjamin-Davies <bentendavies@gmail.com>
2020-09-12 10:24:43 +1200

    Try and space Emoji out more

--- index.ts
+++ index.ts
@@ -1,18 +1,22 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { promises as fs } from 'fs';
 
 const template = 'template.md';
 const destFile = 'README.md';
 
-const section: { [_: string]: string } = {
-  YEAR: `${new Date().getFullYear() - 2008}`,
-  REPOS: 'Coming soon...',
-};
+async function run() {
+  const sections: { [_: string]: string } = {
+    YEAR: `${new Date().getFullYear() - 2008}`,
+    REPOS: 'Coming soon...',
+  };
 
-// Keeping everything in memory should be fine
-// because the file size will be small
-const input = readFileSync(template, { encoding: 'utf8' });
-const output = input.replace(
-  /<!-- PLACEHOLDER:(\w+) -->/g,
-  (_, name: string) => section[name] ?? `<ERROR: section '${name}' not found>`
-);
-writeFileSync(destFile, output);
+  // Keeping everything in memory should be fine
+  // because the file size will be small
+  const input = await fs.readFile(template, { encoding: 'utf8' });
+  const output = input.replace(
+    /<!-- PLACEHOLDER:(\w+) -->/g,
+    (_, name: string) => sections[name] ?? `<ERROR: section '${name}' not found>`
+  );
+  await fs.writeFile(destFile, output);
+}
+
+run();
--- template.md
+++ template.md
@@ -1,6 +1,6 @@
 # Hi there 👋
 
-My name is Benjamin Davies, but you can call me Ben. I am a New Zealander 🥝  who enjoys music 🎸🎷 and scouting ⚜️. I am currently a year-<!-- PLACEHOLDER:YEAR --> who goes to Mount Maunganui College.
+My name is Benjamin Davies, but you can call me Ben. I am a New Zealander 🥝&nbsp;&nbsp;who enjoys music 🎸🎷&nbsp;&nbsp;and scouting ⚜️&nbsp;. I am currently a year-<!-- PLACEHOLDER:YEAR --> who goes to Mount Maunganui College.
 
 Here's what I've been working on:
 
